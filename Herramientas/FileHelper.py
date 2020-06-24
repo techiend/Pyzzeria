37e2782006245e0b58e2f72610f06cdbd9edc286
Carlos Verde <cverde22@gmail.com>
2020-06-24 18:42:58 -0400

    Cambios
    Except y Menu

--- Herramientas/FileHelper.py
+++ Herramientas/FileHelper.py
@@ -34,36 +34,41 @@ def procesar(pedidoPath='pedidos1.pz'):
     line = 0
     t = os.path.isfile(pedidoPath)
     if t == True:
-        with open(pedidoPath, mode="r", encoding="utf-8") as f:
-            f1 = f.readlines()
-            for x in f1:
-                x = fixLine(x)
-                # if x.startswith("COMIENZO_PEDIDO") or x.startswith("FIN_PEDIDO"):
-                if x.startswith("comienzo_pedido") or x.startswith("fin_pedido"):
-                    ''' Reiniciar los contadores al comienzo del pedido '''
-                    line = 1
-                elif line == 1:
-                    ''' La primera linea es nuestra informacion del cliente y la fecha del pedido '''
-                    separa = x.split(";")
-                    nombre = separa[0].strip()
-                    fecha = separa[1].strip()
-                    if nombre == '' or fecha == '':
-                        print('"Nombre" y "Fecha" son los parametros requeridos para crear el Pedido.')
-                        idPedido = 0
-                        line = 0
-                    else:
-                        idPedido = addPedido(nombre, fecha)
-                        # idPedido += 1
-                        line += 1
-                elif line > 1:
-                    if idPedido != 0:
-                        # print(f"Pedido #{idPedido}")
-                        pizza = x.split(";")
-                        tamano = pizza[0].strip()
-                        # print("Tamano: ", tamano)
-                        # print("Ingredientes: ", str(pizza[1:]))
-                        addPizza(idPedido,tamano,pizza[1:])
-                        line += 1
-            print(f"Archivo {pedidoPath} procesado.")
+        try:
+            with open(pedidoPath, mode="r", encoding="utf-8") as f:
+                f1 = f.readlines()
+                for x in f1:
+                    x = fixLine(x)
+                    # if x.startswith("COMIENZO_PEDIDO") or x.startswith("FIN_PEDIDO"):
+                    if x.startswith("comienzo_pedido") or x.startswith("fin_pedido"):
+                        ''' Reiniciar los contadores al comienzo del pedido '''
+                        line = 1
+                    elif line == 1:
+                        ''' La primera linea es nuestra informacion del cliente y la fecha del pedido '''
+                        separa = x.split(";")
+                        nombre = separa[0].strip()
+                        fecha = separa[1].strip()
+                        if nombre == '' or fecha == '':
+                            print('"Nombre" y "Fecha" son los parametros requeridos para crear el Pedido.')
+                            idPedido = 0
+                            line = 0
+                        else:
+                            idPedido = addPedido(nombre, fecha)
+                            # idPedido += 1
+                            line += 1
+                    elif line > 1:
+                        if idPedido != 0:
+                            # print(f"Pedido #{idPedido}")
+                            pizza = x.split(";")
+                            tamano = pizza[0].strip()
+                            # print("Tamano: ", tamano)
+                            # print("Ingredientes: ", str(pizza[1:]))
+                            addPizza(idPedido,tamano,pizza[1:])
+                            line += 1
+                print(f"Archivo {pedidoPath} procesado.")        
+        except FileNotFoundError:
+            print("Lo siento, archivo no encontrado.")
+        except PermissionError:
+            print("Lo siento, no posee permisos para trabajar sobre este archivo")
     else:
         print(f"ERRROR \n¡¡ El archivo {pedidoPath} no existe !!")
--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from Herramientas import *
 
 def printTables():
+    clearScreen()
     cur = CONNECTION.cursor()
 
     '''Muestra los datos de Pedido'''
@@ -67,11 +68,59 @@ def printTables():
     print("-----------------------------------")
     for row in rows:
         print("{} {} {} {}".format(row[0], "      "+str(row[1])+"         ",str(row[2])+"       ", str(row[3])))
-    print('\n')
+    print('\n\n')
+    val = input("presiona una tecla para continuar...")
+
+def clearScreen():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+def cargar():
+    clearScreen()
+    print("---------OPCION---------")
+    print("   1. Cargar pedido     ")
+    print("---------OPCION---------\n")
+    path = input("Ingresa el path del archivo a cargar: ")
+    arch = os.path.isfile(path)
+    if arch == True:
+        procesar(path)
+    else:
+        print(F"Lo siento, el archivo especificado no se encuentra.")
+    val = input("\n\nPresiona una tecla para continuar...")
 
 if __name__ == "__main__":
     # print("MAIN")
-    procesar('pedidos1.pz')
-    procesar('pedidos2.pz')
-    procesar('pedidos3.pz')
-       
+    # procesar('pedidos1.pz')
+    # procesar('pedidos2.pz')
+    # procesar('pedidos3.pz')
+
+    while True:
+        clearScreen()
+        print("----------MENU----------")
+        print("   1. Cargar pedido     ")
+        print("   2. Generar resumen   ")
+        print("   3. Select tablas     ")
+        print("                        ")
+        print("   0. Salir             ")
+        print("----------MENU----------\n")
+        try:
+            opcion = int(input("Introduce una opcion: "))
+            
+            if (opcion == 1):
+                cargar()
+            elif (opcion == 2):
+                clearScreen()
+                print("Generar resumen.")
+                val = input("\n\nPresiona una tecla para continuar...")
+            elif (opcion == 3):
+                printTables()
+            elif (opcion == 0):
+                clearScreen()
+                input("Saliendo de Pyzzeria!\n\nPresiona una tecla para cerrar...")
+                break
+            else:
+                clearScreen()
+        except ValueError:
+            pass
