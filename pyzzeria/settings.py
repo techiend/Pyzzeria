31f69789268ad2fb707b4956e217f323fce15376
Carlos Verde <techiendca@gmail.com>
2020-08-02 22:14:28 -0400

    Inicio App "Pedidos"

--- pyzzeria/settings.py
+++ pyzzeria/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ES'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Caracas'
 
 USE_I18N = True
 
