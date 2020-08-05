c1035929345d1e9767618140349ee1d4c53bc988
Alessandra Varisco <alessandravariscod3@gmail.com>
2020-08-05 00:46:49 -0400

    Adelanto
    Detalle de pedido y factura al final pedido.

--- pedidos/models.py
+++ pedidos/models.py
@@ -5,6 +5,13 @@ class Tamano(models.Model):
     tamano_id = models.AutoField(primary_key=True)
     nombre_tamano = models.CharField(max_length=200)
     costo_tamano = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def get_name(self):
+        return self.nombre_tamano
+
+    def get_costo(self):
+        return self.costo_tamano
+
     def __str__(self):
         return self.nombre_tamano + " | " + str(self.costo_tamano)
 
@@ -12,6 +19,13 @@ class Ingrediente(models.Model):
     ingrediente_id = models.AutoField(primary_key=True)
     nombre_ingrediente = models.CharField(max_length=200)
     costo_ingrediente = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def get_name(self):
+        return self.nombre_ingrediente
+
+    def get_costo(self):
+        return self.costo_ingrediente
+        
     def __str__(self):
         return self.nombre_ingrediente + " | " + str(self.costo_ingrediente)
 
